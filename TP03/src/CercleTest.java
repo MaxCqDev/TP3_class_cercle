ec5a80247df4747209b73e238d88df620752c9e2
MaxCqDev <maximecq.dev@gmail.com>
2026-02-03 22:13:13 +0100

    FINIS

--- TP03/src/CercleTest.java
+++ TP03/src/CercleTest.java
@@ -18,7 +18,7 @@ public class CercleTest {
     private Point A, B, C, D, E;
 
     // Les cercles du sujet
-    private Cercle C1, C2;
+    private Cercle C1, C2, C3;
 
     @Before public void setUp() {
         // Construire les points
@@ -29,9 +29,9 @@ public class CercleTest {
         E = new Point(8, 4);
 
         // Construire les cercles
-        C1 = new Cercle(A, 2.5);
-        C2 = new Cercle(new Point(6, 1), 2);
-        C2.setCouleur(Color.yellow);
+        C1 = new Cercle(C, D); //12
+        C2 = new Cercle(C, D, Color.yellow); //13
+        C3 = Cercle.creerCercle(D,E); //14
     }
 
     /** Vérifier si deux points ont mêmes coordonnées.
@@ -44,8 +44,21 @@ public class CercleTest {
     }
 
     @Test public void testerE12(){
-        memesCoordonnees("E11 : Centre de C1 incorrect", A, C1.getCentre());
+        memesCoordonnees("E12 : centre mauvais", new Point(6, 1), C2.getCentre());
 
+        assertEquals("E12 : rayon mauvais", 2.0, C1.getRayon(), EPSILON );
+        assertEquals("E12 : couleur fausse", Color.blue, C1.getCouleur());
+    }
 
+    @Test public void testerE13() {
+        assertEquals("E13 : couleur doit etre jaune", Color.yellow, C2.getCouleur());
+        assertEquals("E13 : rayon mauvais", 2.0, C2.getRayon(), EPSILON);
+    }
+
+    @Test public void testerE14() {
+        memesCoordonnees("E14 : mauvais centre", D, C3.getCentre());
+
+        assertEquals("E14 : rayon mauvais", 3.0, C3.getRayon(), EPSILON);
+        assertEquals("E14 : mauvaise couleur", Color.blue, C3.getCouleur()) ;
     }
 }
